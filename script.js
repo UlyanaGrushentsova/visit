a21c130fe6e7f0d0b28614012426c125defbbb95
Ulyana <grushencova@mail.ru>
2025-04-06 17:59:47 +0300

    изменила

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const palindrom=str=> {
-    str=str.tolowercase()
-    return str === str.split('').reverse('').join('')
-
-
+str=str.toLowerCase()
+return str === str.split('').reverse('').join('')
 }
 const str="Довод"
 const isPalindrom=palindrom(str)
-console.log(isPalindrom)
-
+if (isPalindrom === true) { 
+  console.log(`Слово ${str} является полиндромом`)
+}
+else {
+  console.log(`Слово ${str} не является полиндромом`)
+}
